3d5fd28fc83732b5e3865e9bbef4ac00e6acb753
AravindKrishnaR <aravindkrishna.r2018@vitstudent.ac.in>
2019-03-22 15:46:19 +0530

    Login page

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+   
+   $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysqli_error());
+   }
+   
+   echo 'Connected successfully';
+
+   
+   $sql = 'SELECT * FROM profile';
+   mysqli_select_db('socialmedia');
+   $retval = mysqli_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not get data: ' . mysqli_error());
+   }
+   
+   while($row = mysqli_fetch_array($retval, MYSQL_ASSOC)) {
+      echo "Id :{$row['id']}  <br> ".
+         "EmailId : {$row['email_id']} <br> ".
+         "DOB : {$row['dob']} <br> ".
+         "--------------------------------<br>";
+   }
+   
+   echo "Fetched data successfully\n";
+   
+   mysqli_close($conn);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +39,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="/CSS/main.css">
     <script src="main.js"></script>
 </head>
 <body>
